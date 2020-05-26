f3c89bceaab2aa075a0bed0cd91cbce22117adf6
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2020-05-26 14:06:08 -0300

    second commit

---
+++ src/shared/container/index.ts
@@ -0,0 +1,17 @@
+import { container } from 'tsyringe';
+
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+
+container.registerSingleton<IAppointmentsRepository>(
+  'AppointmentsRepository',
+  AppointmentsRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
